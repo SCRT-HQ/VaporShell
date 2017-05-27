bf9564a10e273fa813c3b5376d17d9e5e14fdf4a
Nate Ferrell <nate@scrthq.com>
2017-05-27 10:58:15 -0500

    added Add-FnJoin

---
+++ Vaporshell/Public/Add-FnJoin.ps1
@@ -0,0 +1,17 @@
+function Add-FnJoin {
+    [cmdletbinding()]
+    Param
+    (
+        [parameter(Mandatory = $false,Position = 0)]
+        [String]
+        $Delimiter = $null,
+        [parameter(Mandatory = $true,Position = 1)]
+        [object[]]
+        $Values
+    )
+    $obj = New-Object PSObject -Property @{
+        "Fn::Join" = @($Delimiter,@($Values))
+    }
+    Write-Verbose "Resulting JSON from $($MyInvocation.MyCommand): $($obj | ConvertTo-Json -Depth 5 -Compress)"
+    return $obj
+}
