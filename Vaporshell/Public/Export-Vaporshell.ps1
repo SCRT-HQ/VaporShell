f1b777027795e452dffcd879df8e3a49cb160547
Nate Ferrell <nate@scrthq.com>
2017-05-30 21:14:08 -0500

    !deploy fixes to Export-Vaporshell -- using Set-Content over Out-File due to encoding issues

--- Vaporshell/Public/Export-Vaporshell.ps1
+++ Vaporshell/Public/Export-Vaporshell.ps1
@@ -54,7 +54,7 @@ function Export-Vaporshell {
     }
     Process {
         if ($Path) {
-            ConvertTo-Json -Depth 50 -InputObject $VaporshellTemplate -Verbose:$false | Out-File -FilePath $Path @ForcePref -Verbose:$false
+            ConvertTo-Json -Depth 50 -InputObject $VaporshellTemplate -Verbose:$false | Set-Content -Path $Path @ForcePref -Verbose:$false 
         }
         else {
             ConvertTo-Json -Depth 50 -InputObject $VaporshellTemplate -Verbose:$false
