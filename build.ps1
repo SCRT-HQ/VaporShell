31017d40260d5d9bbda84bab6bdfb7a424eccca4
Nate Ferrell <nate@scrthq.com>
2017-06-05 23:15:51 -0500

    !deploy & switching back to Psake for this build

--- build.ps1
+++ build.ps1
@@ -1,10 +1,12 @@
-﻿# Grab nuget bits, install modules, set build variables, start build.
+﻿param($Task = 'Default')
+
+# Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module InvokeBuild, PSDeploy, Pester, BuildHelpers -Force -SkipPublisherCheck -AllowClobber
-Import-Module InvokeBuild, BuildHelpers
+Install-Module Psake, PSDeploy, Pester, BuildHelpers -force
+Import-Module Psake, BuildHelpers
 
 Set-BuildEnvironment
 
-Invoke-Build Build .\.build.ps1
-exit
+Invoke-psake .\psake.ps1 -taskList $Task -nologo
+exit ( [int]( -not $psake.build_success ) )
