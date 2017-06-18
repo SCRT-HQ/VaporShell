080b6a7f4572ab98416efcc14c06218c812abcec
Nate Ferrell <nate@scrthq.com>
2017-06-17 21:13:33 -0700

    updates

---
+++ Vaporshell/Public/Resource Property Types/Add-VSECSTaskDefinitionKeyValuePair.ps1
@@ -0,0 +1,72 @@
+function Add-VSECSTaskDefinitionKeyValuePair {
+    <#
+    .SYNOPSIS
+        Adds an AWS::ECS::TaskDefinition.KeyValuePair resource property to the template
+
+    .DESCRIPTION
+        Adds an AWS::ECS::TaskDefinition.KeyValuePair resource property to the template
+
+    .LINK
+        http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ecs-taskdefinition-containerdefinitions-environment.html
+
+    .PARAMETER Name
+		Documentation: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ecs-taskdefinition-containerdefinitions-environment.html#cfn-ecs-taskdefinition-containerdefinition-environment-name    
+		PrimitiveType: String    
+		Required: False    
+		UpdateType: Immutable    
+
+    .PARAMETER Value
+		Documentation: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-ecs-taskdefinition-containerdefinitions-environment.html#cfn-ecs-taskdefinition-containerdefinition-environment-value    
+		PrimitiveType: String    
+		Required: False    
+		UpdateType: Immutable    
+
+    .FUNCTIONALITY
+        Vaporshell
+    #>
+    [OutputType('Vaporshell.Resource.ECS.TaskDefinition.KeyValuePair')]
+    [cmdletbinding()]
+    Param
+    (
+        [parameter(Mandatory = $false)]
+        [ValidateScript( {
+                $allowedTypes = "System.String","Vaporshell.Function"
+                if ([string]$($_.PSTypeNames) -match "($(($allowedTypes|ForEach-Object{[RegEx]::Escape($_)}) -join '|'))") {
+                    $true
+                }
+                else {
+                    throw "This parameter only accepts the following types: $($allowedTypes -join ", "). The current types of the value are: $($_.PSTypeNames -join ", ")."
+                }
+            })]
+        $Name,
+        [parameter(Mandatory = $false)]
+        [ValidateScript( {
+                $allowedTypes = "System.String","Vaporshell.Function"
+                if ([string]$($_.PSTypeNames) -match "($(($allowedTypes|ForEach-Object{[RegEx]::Escape($_)}) -join '|'))") {
+                    $true
+                }
+                else {
+                    throw "This parameter only accepts the following types: $($allowedTypes -join ", "). The current types of the value are: $($_.PSTypeNames -join ", ")."
+                }
+            })]
+        $Value
+    )
+    Begin {
+        $obj = [PSCustomObject]@{}
+    }
+    Process {
+        foreach ($key in $PSBoundParameters.Keys) {
+            $val = $((Get-Variable $key).Value)
+            if ($val -eq "True") {
+                $val = "true"
+            }
+            elseif ($val -eq "False") {
+                $val = "false"
+            }
+            $obj | Add-Member -MemberType NoteProperty -Name $key -Value $val
+        }
+    }
+    End {
+        $obj | Add-ObjectDetail -TypeName 'Vaporshell.Resource.ECS.TaskDefinition.KeyValuePair'
+    }
+}
