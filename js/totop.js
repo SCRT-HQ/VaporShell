6826ed373148a82365361acd8512bdf2d2e45617
Nate Ferrell <nate@scrthq.com>
2017-06-19 22:17:41 -0700

    updated js

--- js/totop.js
+++ js/totop.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($){
 		$back_to_top = $('.cd-top');
 
 	//hide or show the "back to top" link
-	$(window).scroll(function(){
+	$(#s4-workspace).scroll(function(){
 		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
 		if( $(this).scrollTop() > offset_opacity ) { 
 			$back_to_top.addClass('cd-fade-out');
