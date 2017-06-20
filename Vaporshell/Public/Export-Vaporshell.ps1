f1ca991376c1c8baf3417a7fe8ac33e24ea0be55
Nate Ferrell <nate@scrthq.com>
2017-06-20 11:47:44 -0700

    !deploy Export-Vaporshell now includes the -ValidateTemplate param

--- Vaporshell/Public/Export-Vaporshell.ps1
+++ Vaporshell/Public/Export-Vaporshell.ps1
@@ -34,29 +34,50 @@ function Export-Vaporshell {
     Param
     (
         [parameter(Mandatory = $true,Position = 0,ValueFromPipeline = $true)]
-        [ValidateScript({
-            if($_.Resources) {
-                $true
-            }
-            else {
-                throw "Unable to find any resources on this Vaporshell template. Resources are required in CloudFormation templates at the minimum."
-            }
-        })]
+        [ValidateScript( {
+                if ($_.Resources) {
+                    $true
+                }
+                else {
+                    throw "Unable to find any resources on this Vaporshell template. Resources are required in CloudFormation templates at the minimum."
+                }
+            })]
         [PSTypeName('Vaporshell.Template')]
         $VaporshellTemplate,
         [parameter(Mandatory = $true,Position = 1)]
         [System.String]
         $Path,
+        [parameter(Mandatory = $false)]
+        [Switch]
+        $ValidateTemplate,
+        [parameter(Mandatory = $false)]
         [Switch]
         $Force
     )
     Begin {
+        $Path = (Resolve-Path $Path).Path
         $ForcePref = @{}
         if ($Force) {
             $ForcePref.add("Force",$True)
         }
     }
     Process {
+        Write-Verbose "Exporting template to: $Path"
         ConvertTo-Json -Depth 100 -InputObject $VaporshellTemplate -Verbose:$false | Set-Content -Path $Path @ForcePref -Verbose:$false
+    }
+    End {
+        if ($ValidateTemplate) {
+            if (Get-Command aws) {
+                Write-Verbose "Validating template"
+                $fileUrl = "$($Path.Replace("\","/"))"
+                if ($val = aws cloudformation validate-template --template-body fileb://$fileUrl) {
+                    Write-Host -ForegroundColor Green "The template was validated successfully!`n"
+                    return $val
+                }
+            }
+            else {
+                Write-Warning "AWS CLI tools not found in PATH! Skipping validation to prevent failure."
+            }
+        }
     }
 }
