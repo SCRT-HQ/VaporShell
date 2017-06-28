a14d0ae435ece50c37cacee5a85885ad3c619a06
Nate Ferrell <nate@scrthq.com>
2017-06-28 02:57:45 -0500

    !deploy enhancements for #2 + v1.0.0 release milestone!

--- psake.ps1
+++ psake.ps1
@@ -68,7 +68,8 @@ Task Build -Depends Test {
 
     # Bump the module version
     $Version = Get-NextPSGalleryVersion -Name $env:BHProjectName
-    Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value $Version
+    Update-Metadata -Path $env:BHPSModuleManifest -PropertyName ModuleVersion -Value "1.0.0" #$Version
+    
     #Update-Metadata -Path $env:BHPSModuleManifest -Increment Build
 }
 
