c60dc26e2a5f6b9bc6ac94fd12e02e3fc629792c
Nate Ferrell <nate@scrthq.com>
2017-07-02 13:42:04 -0700

    !deploy module load time fix #5

--- Vaporshell/Vaporshell.psm1
+++ Vaporshell/Vaporshell.psm1
@@ -2,16 +2,29 @@
 $Public = @( Get-ChildItem -Path $PSScriptRoot\Public -Recurse -Filter "*.ps1" -ErrorAction SilentlyContinue )
 $Private = @( Get-ChildItem -Path $PSScriptRoot\Private -Recurse -Filter "*.ps1" -ErrorAction SilentlyContinue )
 
-#Dot source the files
-Foreach ($import in @($Public + $Private)) {
-    . $import.fullname
+#Execute a scriptblock to load each function instead of  (Issue #5)
+foreach ($file in @($Public + $Private)) {
+    $ExecutionContext.InvokeCommand.InvokeScript(
+        $false, 
+        (
+            [scriptblock]::Create(
+                [io.file]::ReadAllText(
+                    $file.FullName,
+                    [Text.Encoding]::UTF8
+                )
+            )
+        ), 
+        $null, 
+        $null
+    )
 }
+
 
 # Add in Pseudo Parameter variables from private text file (allows growth in case additional parameters need to be added in)
 $vars = @()
 Get-Content -Path "$PSScriptRoot\Private\PseudoParams.txt" | ForEach-Object {
     $name = "_$(($_ -replace "::").Trim())"
-    New-Variable -Name $name -Value $_
+    New-Variable -Name $name -Value "$(($_).Trim())"
     $vars += $name
 }
 
