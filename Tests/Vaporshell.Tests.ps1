91c4d72144e2c0d7777a71ddd005fcc6d1eb3879
Nate Ferrell <nate@scrthq.com>
2017-07-02 23:56:07 -0500

    updating Appveyor tests

--- Tests/Vaporshell.Tests.ps1
+++ Tests/Vaporshell.Tests.ps1
@@ -172,38 +172,14 @@ Describe "Initialize/Export/Import PS$PSVersion" {
                     "Description"           = "VpcId of your existing Virtual Private Cloud (VPC)2"
                     "ConstraintDescription" = "must be the VPC Id of an existing Virtual Private Cloud.2"
                 }
-            $template.AddParameter((New-VaporParameter @vp1Params -))
+            $template.AddParameter((New-VaporParameter @vp1Params))
             $template.AddParameter((New-VaporParameter @vp2Params))
-            $template.AddMetadata((New-VaporMetadata -LogicalId "Instances" -Metadata @{"Description" = "Information about the instances"}))
-            $template.AddCondition(
-                (New-VaporCondition -LogicalId "CreateProdResources" -Condition (Add-ConEquals -FirstValue (Add-FnRef -Ref "EnvType") -SecondValue "prod")),
-                (Add-Include -Location "s3://MyAmazonS3BucketName/single_wait_condition.yaml")
-            )
-            $template.AddCondition(
-                (New-VaporCondition -LogicalId "CreateProdResources2" -Condition (Add-ConEquals -FirstValue (Add-FnRef -Ref "EnvType2") -SecondValue "prod"))
-            )
-            $template.AddCondition(
-                (New-VaporCondition -LogicalId "CreateBothProdResources" -Condition (Add-ConAnd -Conditions (Add-FnRef "CreateProdResources"),(Add-FnRef "CreateProdResources2" )))
-            )
-            $template.AddMapping(
-                (New-VaporMapping -LogicalId "RegionMap" -Map ([PSCustomObject][Ordered]@{
-                        "us-east-1" = [PSCustomObject][Ordered]@{
-                            "32" = "ami-6411e20d"
-                            "64" = "ami-7a11e213"
-                        }
-                        "us-west-1" = [PSCustomObject][Ordered]@{
-                            "32" = "ami-c9c7978c"
-                            "64" = "ami-cfc7978a"
-                        }
-                    })
-                )
-            )
             $template.AddResource(
-                (New-VSApiGatewayDeployment -LogicalId "GatewayDeployment" -Description "My deployment" -RestApiId (Add-FnRef -Ref "MyApi") -StageDescription (Add-VSApiGatewayDeploymentStageDescription -MethodSettings (Add-VSApiGatewayDeploymentMethodSetting -LoggingLevel ERROR) -CacheClusterEnabled $true -CacheDataEncrypted $false)),
-                (New-VSApiGatewayRestApi -LogicalId "MyApi" -Description "My REST API"),
-                (New-VSEC2Instance -LogicalId "MyInstance" -AvailabilityZone "us-east-1a" -ImageId (Add-FnFindInMap -MapName "RegionMap" -TopLevelKey "us-west-1" -SecondLevelKey "32") -Condition "CreateProdResources" -Tags (Add-VSTag -Key "Name" -Value "MyInstance"),(Add-VSTag -Key "Environment" -Value "Production") -CreationPolicy (Add-CreationPolicy -MinSuccessfulInstancesPercent 100 -Count 1 -Timeout "PT5M") -UpdatePolicy (Add-UpdatePolicy -WillReplace $true -MaxBatchSize 2 -MinInstancesInService 2 -MinSuccessfulInstancesPercent 100 -PauseTime "PT30S" -WaitOnResourceSignals $true -IgnoreUnmodifiedGroupSizeProperties $true) -DeletionPolicy Delete -DependsOn "GatewayDeployment" -Metadata ([PSCustomObject]@{CommonName = "WebServer1"}) -UserData (Add-UserData -File $udFile))
+                (New-VSApiGatewayDeployment -LogicalId "GatewayDeployment3" -Description "My deployment" -RestApiId (Add-FnRef -Ref "MyApi") -StageDescription (Add-VSApiGatewayDeploymentStageDescription -MethodSettings (Add-VSApiGatewayDeploymentMethodSetting -LoggingLevel ERROR) -CacheClusterEnabled $true -CacheDataEncrypted $false)),
+                (New-VSApiGatewayRestApi -LogicalId "MyApi3" -Description "My REST API"),
+                (New-VSEC2Instance -LogicalId "MyInstance3" -AvailabilityZone "us-east-1a" -ImageId (Add-FnFindInMap -MapName "RegionMap" -TopLevelKey "us-west-1" -SecondLevelKey "32") -Condition "CreateProdResources" -Tags (Add-VSTag -Key "Name" -Value "MyInstance"),(Add-VSTag -Key "Environment" -Value "Production") -CreationPolicy (Add-CreationPolicy -MinSuccessfulInstancesPercent 100 -Count 1 -Timeout "PT5M") -UpdatePolicy (Add-UpdatePolicy -WillReplace $true -MaxBatchSize 2 -MinInstancesInService 2 -MinSuccessfulInstancesPercent 100 -PauseTime "PT30S" -WaitOnResourceSignals $true -IgnoreUnmodifiedGroupSizeProperties $true) -DeletionPolicy Delete -DependsOn "GatewayDeployment3" -Metadata ([PSCustomObject]@{CommonName = "WebServer1"}) -UserData (Add-UserData -File $udFile))
             )
-            $template.AddOutput((New-VaporOutput -LogicalId "BackupLoadBalancerDNSName" -Description "The DNSName of the backup load balancer" -Value (Add-FnGetAtt -LogicalNameOfResource "BackupLoadBalancer" -AttributeName "DNSName") -Condition "CreateProdResources"))
+            $template.AddOutput((New-VaporOutput -LogicalId "BackupLoadBalancerDNSName3" -Description "The DNSName of the backup load balancer" -Value (Add-FnGetAtt -LogicalNameOfResource "BackupLoadBalancer3" -AttributeName "DNSName") -Condition "CreateProdResources"))
 
             Export-Vaporshell -VaporshellTemplate $template -Path $testPath -Force
         }
