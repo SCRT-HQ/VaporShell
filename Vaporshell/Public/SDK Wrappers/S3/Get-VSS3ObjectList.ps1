827deab3a941707ff70479f0369d3814e1042415
Nate Ferrell <nate@scrthq.com>
2017-09-27 11:35:29 -0700

    added Get-VSS3ObjectList, bumped to v2.0.3

--- Vaporshell/Public/SDK Wrappers/S3/Get-VSS3ObjectList.ps1
+++ Vaporshell/Public/SDK Wrappers/S3/Get-VSS3ObjectList.ps1
@@ -1,13 +1,19 @@
-function Get-VSS3ObjectMetadata {
+function Get-VSS3ObjectList {
     <#
     .SYNOPSIS
-    Gets an S3 object's metadata
+    Gets the objects from a specific S3 bucket
     
     .PARAMETER BucketName
     The name of the bucket containing the object.
     
-    .PARAMETER Key
-    The name (including prefix, if applicable) of the object.
+    .PARAMETER Prefix
+    Limits the response to keys that begin with the specified prefix.
+    
+    .PARAMETER StartAfter
+    StartAfter is where you want Amazon S3 to start listing from. Amazon S3 starts listing after this specified key. StartAfter can be any key in the bucket.
+    
+    .PARAMETER MaxKeys
+    Sets the maximum number of keys returned in the response. The response might contain fewer keys but will never contain more.
     
     .PARAMETER ProfileName
     The name of the configuration profile to deploy the stack with. Defaults to $env:AWS_PROFILE, if set.
@@ -21,15 +27,22 @@ function Get-VSS3ObjectMetadata {
         [parameter(Mandatory = $true,Position = 0)]
         [String]
         $BucketName,
-        [parameter(Mandatory = $true,Position = 1)]
+        [parameter(Mandatory = $false)]
         [String]
-        $Key,
+        $Prefix,
+        [parameter(Mandatory = $false)]
+        [String]
+        $StartAfter,
+        [parameter(Mandatory = $false)]
+        [System.Int32]
+        $MaxKeys,
         [parameter(Mandatory = $false)]
         [String]
         $ProfileName = $env:AWS_PROFILE
     )
     Process {
-        $method = "GetObjectMetadata"
+        $method = "ListObjectsV2"
+        $expand = "S3Objects"
         $requestType = "Amazon.S3.Model.$($method)Request"
         $request = New-Object $requestType
         foreach ($key in $PSBoundParameters.Keys) {
@@ -37,6 +50,7 @@ function Get-VSS3ObjectMetadata {
                 $request.$key = $PSBoundParameters[$key]
             }
         }
+        $request.FetchOwner = $true
         $results = ProcessRequest $PSCmdlet.ParameterSetName $ProfileName $method $request $expand
         if (!$results) {
             return
@@ -45,7 +59,7 @@ function Get-VSS3ObjectMetadata {
             $PSCmdlet.ThrowTerminatingError($results)
         }
         elseif ($results) {
-            return $results
+            return $results | Select-Object *,@{N="OwnerDisplayName";E={$_.Owner.DisplayName}} -ExcludeProperty Owner
         }
     }
 }
