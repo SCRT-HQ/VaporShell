cecd4f8182e0cac9bd05262e67f16d93cdc85318
Nate Ferrell <nate@scrthq.com>
2018-03-12 00:52:54 -0500

    !build corrected folder structure and add Resolve-Path part 3 with fixed path resolution

--- Tests/Vaporshell.Tests.ps1
+++ Tests/Vaporshell.Tests.ps1
@@ -1,7 +1,7 @@
 $PSVersion = $PSVersionTable.PSVersion.Major
 $ModuleName = "VaporShell"
 $projectRoot = Resolve-Path "$PSScriptRoot\.."
-$ModulePath = Join-Path (Join-Path $projectRoot $ModuleName) "$($ModuleName).psd1"
+$ModulePath = Resolve-Path "$projectRoot\*\*.psd1"
 
 # Verbose output for non-master builds on appveyor
 # Handy for troubleshooting.
