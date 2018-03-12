3c9b3bda74ceb6ee017f115d0a3f206faca8da55
Nate Ferrell <nate@scrthq.com>
2018-03-12 00:12:27 -0500

    !build updated appveyor.yml to specify build commands per OS

--- Vaporshell/Vaporshell.psm1
+++ Vaporshell/Vaporshell.psm1
@@ -40,7 +40,7 @@ else {
 
 # Add in Pseudo Parameter variables from private text file (allows growth in case additional parameters need to be added in)
 $vars = @()
-$varDict = . (Join-Path (Join-Path $VaporshellPath "Private") "PseudoParams.ps1")
+$varDict = . (Join-Path (Join-Path $PSScriptRoot "Private") "PseudoParams.ps1")
 ForEach ($key in $varDict.Keys) {
     New-Variable -Name $varDict[$key] -Value $key
     $vars += $varDict[$key]
