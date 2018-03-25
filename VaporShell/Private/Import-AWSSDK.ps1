7b2fd0b183fef8c1fac5542c1e48c533543482ae
Nate Ferrell <nate@scrthq.com>
2018-03-25 18:56:29 -0500

    !build update to Import-AWSSDK

--- VaporShell/Private/Import-AWSSDK.ps1
+++ VaporShell/Private/Import-AWSSDK.ps1
@@ -1,31 +1,25 @@
 function Import-AWSSDK {
     [cmdletbinding()]
     Param()
-    try {
-        New-Object Amazon.Runtime.CredentialManagement.CredentialProfileOptions -ErrorAction SilentlyContinue | Out-Null
-        if ($PSVersionTable.PSVersion.Major -ge 6) {
+    if (!([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object {$_.Location -like "*AWSSDK.CloudFormation.dll"})) {
+        if ($IsCoreCLR) {
+            Write-Verbose "Loading AWS SDK for *NetCore*!"
+            $sdkPath = (Join-Path $Script:VaporshellPath "bin\NetCore" -Resolve)
+        }
+        else {
+            Write-Verbose "Loading AWS SDK for *Net45*!"
+            $sdkPath = (Join-Path $Script:VaporshellPath "bin\Net45" -Resolve)
+        }
+        Get-ChildItem $sdkPath -Filter "*.dll" | ForEach-Object {
+            [reflection.assembly]::LoadFrom("$($_.FullName)") | Out-Null
+        }
+    }
+    else {
+        if ($IsCoreCLR) {
             Write-Verbose "AWS SDK for *NetCore* is already loaded, continuing module import"
         }
         else {
             Write-Verbose "AWS SDK for *Net45* is already loaded, continuing module import"
-        }
-    }
-    catch {
-        if ($PSVersionTable.PSVersion.Major -ge 6) {
-            Write-Verbose "Loading AWS SDK for *NetCore*!"
-            $sdkPath = (Join-Path (Join-Path $Script:VaporshellPath "bin") "NetCore" -Resolve)
-        }
-        else {
-            Write-Verbose "Loading AWS SDK for *Net45*!"
-            $sdkPath = (Join-Path (Join-Path $Script:VaporshellPath "bin") "Net45" -Resolve)
-        }
-        Get-ChildItem $sdkPath -Filter "*.dll" | ForEach-Object {
-            $dll = $_          
-            $fileStream = ([System.IO.FileInfo]$dll).OpenRead()
-            $assemblyBytes = New-Object -TypeName byte[] -ArgumentList $fileStream.Length
-            $fileStream.Read($assemblyBytes, 0, $fileStream.Length) | Out-Null
-            $fileStream.Close()
-            [System.Reflection.Assembly]::Load($assemblyBytes) | Out-Null
         }
     }
 }
