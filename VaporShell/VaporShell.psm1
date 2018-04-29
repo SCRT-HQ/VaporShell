94e21e1c8a5b8de3ba786dedab64cfe090664d60
Nate Ferrell <nate@scrthq.com>
2018-04-29 01:37:27 -0500

    !build updated names and DSL module

--- VaporShell/VaporShell.psm1
+++ VaporShell/VaporShell.psm1
@@ -31,11 +31,18 @@ foreach ($file in @($Public + $Private)) {
         )
     }
 }
-if ($env:CI -or $QuietLoad) {
-    Import-AWSSDK
-}
-else {
-    Import-AWSSDK -Verbose
+if (!([AppDomain]::CurrentDomain.GetAssemblies() | Where-Object {$_.Location -like "*AWSSDK.CloudFormation.dll"})) {
+    if ($IsCoreCLR) {
+        Write-Verbose "Loading AWS SDK for *NetCore*!"
+        $sdkPath = (Join-Path $Script:VaporshellPath "bin\NetCore" -Resolve)
+    }
+    else {
+        Write-Verbose "Loading AWS SDK for *Net45*!"
+        $sdkPath = (Join-Path $Script:VaporshellPath "bin\Net45" -Resolve)
+    }
+    Get-ChildItem $sdkPath -Filter "*.dll" | ForEach-Object {
+        [reflection.assembly]::LoadFrom("$($_.FullName)") | Out-Null
+    }
 }
 
 # Add Intrinsic Function short aliases
