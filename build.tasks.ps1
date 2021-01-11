767092b5f12b6fb93f5d4644f861828a6887058e
Nate Ferrell <nate@scrthq.com>
2021-01-11 00:52:35 -0600

    set AllowPrerelease=$true on Publish-Module parameters

--- build.tasks.ps1
+++ build.tasks.ps1
@@ -801,6 +801,9 @@ Task PublishToPSGallery -If $psGalleryConditions {
             Repository = 'PSGallery'
             Verbose = $true
         }
+        if ($env:BHBranchName -notin @('master','main')){
+            $pars['AllowPrerelease'] = $true
+        }
         Publish-Module @pars
     }
     Write-BuildLog "Deployment successful!"
