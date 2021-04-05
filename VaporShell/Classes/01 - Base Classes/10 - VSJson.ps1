8cb97cb41c9399836208623695e5755d15a54790
Nate Ferrell <nate@scrthq.com>
2021-04-04 20:13:31 -0500

    updated VSJson to be 5.1 compatible

--- VaporShell/Classes/01 - Base Classes/10 - VSJson.ps1
+++ VaporShell/Classes/01 - Base Classes/10 - VSJson.ps1
@@ -3,50 +3,8 @@ using namespace System.Collections
 using namespace System.Collections.Generic
 using namespace System.IO
 using namespace System.Management.Automation
+using namespace System.Web
 
-<# class VSJson : VSHashtable {
-    static [VSJson] Transform([string] $jsonStringOrFilePath) {
-        $newData = if (Test-Path $jsonStringOrFilePath) {
-            $resolvedPath = (Resolve-Path $jsonStringOrFilePath).Path
-            [File]::ReadAllText($resolvedPath)
-        }
-        else {
-            $jsonStringOrFilePath
-        }
-        try {
-            $final = (ConvertFrom-Json -InputObject $newData -ErrorAction Stop)
-        }
-        catch {
-            throw [VSError]::InvalidJsonInput($jsonStringOrFilePath)
-        }
-        $dict = [ordered]@{}
-        $final.PSObject.Properties | ForEach-Object {
-            $dict[$_.Name] = $_.Value
-        }
-        return [VSJson]::new($dict)
-    }
-
-    VSJson() : base() {}
-    VSJson([IDictionary] $dictionary) {
-        $dictionary.GetEnumerator() | ForEach-Object {
-            $this[$_.Key] = $_.Value
-        }
-    }
-    VSJson([psobject] $psObject) {
-        $psObject.PSObject.Properties | ForEach-Object {
-            $this[$_.Name] = $_.Value
-        }
-    }
-    VSJson([string] $jsonStringOrFilePath) {
-        $this = $this::Transform($jsonStringOrFilePath)
-    }
-    VSJson([string[]] $jsonStrings) {
-        $newString = $jsonStrings -join [Environment]::NewLine
-        $this = $this::Transform($newString)
-    }
-}
-
-<#  #>
 class VSJson : VSHashtable {
     static [Specialized.OrderedDictionary] TransformToDict([string] $stringOrFilepath) {
         $dictionary = [ordered]@{}
@@ -55,8 +13,7 @@ class VSJson : VSHashtable {
         }
         if ($stringOrFilepath.Trim() -match '^%\d\w') {
             Write-Debug "String appears to be URL encoded, decoding with System.Web.HttpUtility::UrlDecode"
-            Add-Type -AssemblyName System.Web
-            $stringOrFilepath = [System.Web.HttpUtility]::UrlDecode($stringOrFilepath)
+            $stringOrFilepath = [HttpUtility]::UrlDecode($stringOrFilepath)
         }
         try {
             $final = (ConvertFrom-Json -InputObject $stringOrFilepath -ErrorAction Stop)
@@ -114,4 +71,4 @@ class VSJson : VSHashtable {
             $this[$_.Key] = $_.Value
         }
     }
-} #>
+}
